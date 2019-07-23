775441bbb2413cc5332898ca1641910f04938c64
bonjour520 <l>
2019-07-23 14:55:04 +0800

    （一）spring cloud 服务注册中心

---
+++ eureka-server/src/main/java/com/lanceyi/study/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.lanceyi.study.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Lance yi
+ * @date 2019-7-23
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
