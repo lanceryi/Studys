b5f0032497eeaf53b3959b19568b0b30b6ad9aec
bonjour520 <l>
2019-07-24 09:03:50 +0800

    init

---
+++ iess/src/main/java/com/lanceyi/iess/IessApplication.java
@@ -0,0 +1,13 @@
+package com.lanceyi.iess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IessApplication.class, args);
+    }
+
+}
